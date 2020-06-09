f69d4e6cb6909b36597d9daf33fe59b6d5a8f1e5
Divya Kelaskar <divyakelaskar@gmail.com>
2020-06-09 13:03:54 +0530

    changed descrpt quotes

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta property="og:title" content={title || `Divya, learns & codes`} />
         <meta property="og:site_name" content="Divya, learns & codes" />
-        <meta property="og:description" content={og ? og.description : `Writing about the tips, journey that I come across and some more.`} />
+        <meta property="og:description" content={og ? og.description : "Writing about the tips, journey that I come across and some more."} />
         <meta property="og:image" content={og ? og.image : `https://i.ibb.co/pX02n0T/default.jpg`} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@theavidcoder" />
