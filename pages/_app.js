b0ea1c6705ae88de74205185874175c136535808
Divya Kelaskar <divyakelaskar@gmail.com>
2020-06-17 18:43:55 +0530

    .

--- pages/_app.js
+++ pages/_app.js
@@ -22,15 +22,6 @@ function MyApp({ Component, pageProps }) {
 
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168668028-1"></script>
-
-        <script>
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments)};
-         gtag('js', new Date());
-         gtag('config', 'UA-168668028-1');
-        </script>
-
         <title>{title || `Divya, learns & codes`}</title>
       </Head>
 
